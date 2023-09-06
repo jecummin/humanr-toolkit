403e4a51dbdb5a2ff79102d59a71ae2f63c9933b
Jesse E Cummings <jecummin@csail.mit.edu>
2023-09-06 16:05:14 -0400

    Update environment script

--- set_environment.sh
+++ set_environment.sh
@@ -1,6 +1,8 @@
-export REDIS_PORT=<redis port>
-export REDIS_HOST=<redis host>
-export REDIS_PASSWORD=<redis password>
-export COMPARISON_TASK_DATABASE=<database number>
-export COMPARISON_TASK_HOST=<hostname>
-export COMPARISON_TASK_PORT=<port number>
+export REDIS_PORT= # port that redis is running on 
+export REDIS_HOST= # host that redis is runnin from, 127.0.0.1 for local connections
+export REDIS_PASSWORD= # redis password
+export COMPARISON_TASK_DATABASE= # redis db number to collect results in 
+export COMPARISON_TASK_HOST= # hostname for server to collect results
+export COMPARISON_TASK_PORT= # port for server to collect results
+export INSTITUTION_NAME= # name of university/lab conducting experiment
+export INSTITUTION_EMAIL= # email for MTurk workers to contact with questions
